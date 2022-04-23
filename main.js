eb763b2b2b25ca708ac28ecf0c2a4b0f47017097
Mila-Kostic <milakostic.eng@gmail.com>
2022-04-23 20:32:40 +0200

    json

--- main.js
+++ main.js
@@ -1,1 +1,29 @@
-alert("Happy codding!")
+alert("Happy codding!");
+
+let users = [
+    {
+        name:"danilo",
+        status:"admin",
+    },
+    {
+        name:"zarko",
+        status:"bot",
+    },
+    {
+        name:"marko",
+        status:"admin",
+    },
+    {
+        name:"darko",
+        status: "user",
+    },
+    {
+        name:"marko",
+        status: "guest",
+        age:35,
+    }
+    {
+        name: "marko",
+        status: "user"
+    },
+];
