a1a5d33a735b456cb4be8c32ac7260512b7f5c75
Mila-Kostic <milakostic.eng@gmail.com>
2022-04-24 02:32:34 +0200

    m

--- main.js
+++ main.js
@@ -27,3 +27,55 @@ let users = [
         status: "user"
     },
 ];
+
+init();
+
+function init() {
+    let db = {
+        userName: null,
+        returnedUsers: [],
+        config: {
+            status: ["admin", "user", "guest", "bot"],
+            setProps: ["godine", "posao", "grad"],
+        }, 
+        search: function (usersDb) {
+            this.returnersUsers = userDb.filter(
+                (user) => user.name === this.user.userName
+            );
+            console.log("Pretraga u toku...");
+            setTimeout(existUser, 2000);
+        },
+        add: function (usersDb, data) {
+            usersDb.push(data);
+            this.returnedUsers = usersDb;
+        },
+        getAll:function (usersDb) {
+            this.returnedUsers = usersDb;
+        },
+    }
+}
+
+db.userName = prompt("pretrazi korisnika")č
+db.search(users);
+
+function existUser() { 
+    if (db.returnedUsers.length === 0) {
+        addUser();
+    } else {
+        displayUsers();
+    }
+}
+
+function displayUsers() {
+    console.log(`U bazi su pronadjena ${db.returnedUsers.length}
+    korisnika:`);
+    db.returnedUsers.foreach((user, index) => {
+        letinfo = "";
+        // foor loop jer se mo\e desiti da nemaju svi user-i iste property-je
+for (key in user) {
+    if (user[key] !== null) {
+        info += key + ": " + user[key] + " - ";
+    }
+}
+    });
+}
